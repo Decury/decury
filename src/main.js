9efd1c265bd19103e63ba1768757d51bf3610eef
Arda Çebi <ardacebi1@gmail.com>
2020-03-20 21:20:49 +0300

    delete fa 5

--- src/main.js
+++ src/main.js
@@ -17,16 +17,5 @@
 
 import Vue from 'vue'
 import App from './App.vue'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
-import { faSchool } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-
-library.add(faUserSecret)
-library.add(faSchool)
- 
-Vue.component('font-awesome-icon', FontAwesomeIcon)
- 
-Vue.config.productionTip = false
 
 new Vue({ render: createElement => createElement(App) }).$mount('#app');
